39caeab86edb343539eda1b46e63679eb391e76c
윤정인 <yoonjeongin@naver.com>
2021-09-08 23:42:16 +0900

    add nodemon & html request

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+//서버 불러오는 기본 코드
 const express = require('express');
 const app = express();
-
 app.listen(8080, function(){
    console.log('listening on 8080');
 });
 
+//get 
 app.get('/pet', function(req, res){
     res.send('펫 용품 쇼핑 사이트입니다');
 });
@@ -12,4 +13,8 @@ app.get('/pet', function(req, res){
 //과제부분
 app.get('/beauty', function(req, res){
     res.send('뷰티용품 쇼핑 사이트입니다');
+});
+
+app.get('/', function(req, res){
+   res.sendFile(__dirname + '/index.html');
 });
