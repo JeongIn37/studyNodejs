38306121908dee02aecbd7d8a4fd537966cd19b1
윤정인 <yoonjeongin@naver.com>
2021-09-21 20:53:11 +0900

    post request + bodyParser

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 //서버 불러오는 기본 코드
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({extended: true})) 
+
+/*
+2021년 이후로 설치한 프로젝트들은 body-parser 라이브러리가 express에 기본 포함되어있어서 따로 npm으로 설치할 필요가 없으므로 아래 두 줄 대신 "app.use(express.urlencoded({extended: true}))" 써줌
+const bodyParser= require('body-parser')
+app.use(bodyParser.urlencoded({extended: true})) */
+
+
 app.listen(8080, function(){
    console.log('listening on 8080');
 });
@@ -18,3 +26,12 @@ app.get('/beauty', function(req, res){
 app.get('/', function(req, res){
    res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/write', function(req, res){
+    res.sendFile(__dirname + '/write.html');
+ });
+
+ app.post('/add', function(req, res){
+    console.log(req.body);
+    res.send('전송완료')
+  });
